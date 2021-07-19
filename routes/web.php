6b761ea05a236506d8d92adaea597e02d2559cc0
antonysyahputra <anto79roy@gmail.com>
2021-07-19 14:53:18 +0700

    commit pertama dari local

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts', [
+        "title" => "Blog",
+        "posts" => Post::all()
+    ]);
+});
+
+Route::get('posts/{slug}', function($slug) {
+    return view('post', [
+        "title" => "Single Post",
+        "post" => Post::find($slug)
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "nama" => "Antony Syahputra",
+        "email" => "anto@gmail.com",
+        "image" => "antony.jpg",
+    ]);
+});
