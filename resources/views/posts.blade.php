b275aa5ba41cb742127222d7478d8305ca654f5e
antonysyahputra <anto79roy@gmail.com>
2021-07-30 19:23:30 +0700

    resolving N+1 Problem

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
+<h1 class="mb-5">{{ $title }}</h1>
 
 @foreach ($posts as $post)
 <article class="mb-3 border-bottom">
     <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
-    <p>By. <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    <p>By. <a href="/authors/{{ $post->user->username }}">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
     <p>{{ $post->exerpt }}</p>  
     
     <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
